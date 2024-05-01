96dac1510c69ed600ff45e498e74b8223a454bc6
VChapdelaineT <96508889+VChapdelaineT@users.noreply.github.com>
2024-05-01 15:18:00 -0400

    Update XX_Males_missing.sh

--- Genotype_processing/3_XX_Males_missing/XX_Males_missing.sh
+++ Genotype_processing/3_XX_Males_missing/XX_Males_missing.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 
 ## Declare Variable
-declare -a arrays=("17k" "5300" "4224" "760" "archi")
-path='Mooser_433651_genotypes_hg38_ref'
+
+path=“/path/to/plink_files/*bim”  # path to CAG PLINK binary format's bim file, each set should contain a .bed .bim .fam file.
 
 ## Set HH-missing
-for array in "${arrays[@]}"
+for bim in ${path}
 do
-        plink --bfile ${path}/${array}_hg38_renamed --split-x 'b38'  --keep-allele-order --make-bed --output-chr chrMT --out ${array}_hg38_Xsplit
-        plink --bfile ${array}_hg38_Xsplit --keep-allele-order --set-hh-missing --make-bed --output-chr chrMT --out ${array}_hg38_Xsplit_temp
-        plink --bfile ${array}_hg38_Xsplit_temp --merge-x --keep-allele-order --make-bed --output-chr chrMT --out ${array}_hg38
+        plink --bfile ${bim%.*} --split-x 'b38'  --keep-allele-order --make-bed --output-chr chrMT --out ${bim%.*}_Xsplit
+        plink --bfile ${bim%.*}_Xsplit --keep-allele-order --set-hh-missing --make-bed --output-chr chrMT --out ${bim%.*}_Xsplit_temp
+        plink --bfile ${bim%.*}_Xsplit_temp --merge-x --keep-allele-order --make-bed --output-chr chrMT --out ${bim%.*}_hh
 done
 
 ## Verfication X region
 
-cat *_hg38_Xsplit_temp.hh | grep 'chrX' | cut -f 3 | sort -V | uniq ## head and tail
-cat *_hg38_Xsplit_temp.hh | grep 'chrY' | cut -f 3 | sort -V | uniq ## head and tail
+cat *_Xsplit_temp.hh | grep 'chrX' | cut -f 3 | sort -V | uniq ## head and tail
+cat *_Xsplit_temp.hh | grep 'chrY' | cut -f 3 | sort -V | uniq ## head and tail
 
 ## Verficiation X male Y female
+for bim in ${path}
+do
+        cat ${bim%.*}.fam | cut -d " " -f 1,5 | grep 2$ | cut -d " " -f 1 | sort | uniq >> female.txt
+        cat ${bim%.*}.fam | cut -d " " -f 1,5 | grep 1$ | cut -d " " -f 1 | sort | uniq >> male.txt
+done
 
-cat ${path}/*_hg38_renamed.fam | cut -d " " -f 1,5 | grep 2$ | cut -d " " -f 1 | sort | uniq > female.txt
-cat ${path}/*_hg38_renamed.fam | cut -d " " -f 1,5 | grep 1$ | cut -d " " -f 1 | sort | uniq > male.txt
-
-cat *_hg38_Xsplit_temp.hh | grep 'chrX' | cut -f 1 | sort | uniq > X_ind.hh
-cat *_hg38_Xsplit_temp.hh | grep 'chrY' | cut -f 1 | sort | uniq > Y_ind.hh
+cat *)Xsplit_temp.hh | grep 'chrX' | cut -f 1 | sort | uniq > X_ind.hh
+cat *_Xsplit_temp.hh | grep 'chrY' | cut -f 1 | sort | uniq > Y_ind.hh
 
 grep -v -f male.txt X_ind.hh ## expected : no output
 grep -f female.txt X_ind.hh ## expected : no output
 grep -f female.txt Y_ind.hh ## expected : no output
 
 ####### clean up ####
-for i in archi 4224 5300 760 17k
+for bim in ${path}
 do
-rm "$i"_hg38_Xsplit_temp.*
-rm "$i"_hg38_Xsplit*
+        rm ${bim%.*}_Xsplit_temp.*
+        rm ${bim%.*}_hg38_Xsplit*
 done
