a4379a946bdc52b2b486e4eea675e88f5f85ce25
VChapdelaineT <96508889+VChapdelaineT@users.noreply.github.com>
2024-05-01 13:54:51 -0400

    Update preprocessing.sh

--- Genotype_processing/0_preprocessing/preprocessing.sh
+++ Genotype_processing/0_preprocessing/preprocessing.sh
@@ -3,5 +3,5 @@
 path=“/path/to/plink_files/*bim”  # path to CAG PLINK binary format's set, each set should contain a .bed .bim .fam file.
 remove_samples_file='/path/to/list_of_samples_to_remove/file.samples' # path to the list of samples to remove
 
-for set in ${path}; do 
-        plink -bfile ${set%.*} --remove ${remove_samples_file} --make-bed --output-chr MT --out ${set%.*}_consented
+for bim in ${path}; do 
+        plink -bfile ${bim%.*} --remove ${remove_samples_file} --make-bed --output-chr MT --out ${bim%.*}_consented
