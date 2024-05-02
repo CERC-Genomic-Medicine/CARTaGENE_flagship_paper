0df5848434d1b624ed4f9c12ffa64bc3e2f197fb
VChapdelaineT <96508889+VChapdelaineT@users.noreply.github.com>
2024-05-02 16:29:04 -0400

    Update AF_TOPMed.sh

--- Genotype_processing/6_AF_TOPMed/AF_TOPMed.sh
+++ Genotype_processing/6_AF_TOPMed/AF_TOPMed.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+CaG="Path/to/file.bim" # path to a CAG (harmonized and merged) PLINK binary format's bim file, each should contain a set of .bed .bim .fam file.
+Bravo="Path/to/file.vcf" # path to TOPMed's freeze 8 
+Threshold=0.2 # Frequency concordance threshold
 
-plink --bfile CARTaGENE_hg38_Harmonized  --keep-allele-order --freq --out CARTaGENE_hg38_Harmonized
-python3 plink_freq_vs_topmed.py -s CARTaGENE_hg38_Harmonized.frq -tALL.BRAVO_TOPMed_Freeze_8.vcf.gz -m 0.2 -o Diff_02.txt
-plink --bfile CARTaGENE_hg38_Harmonized --exclude Diff_02.txt.af_diff.txt --keep-allele-order --make-bed --out CARTaGENE_hg38_shared_TOPMed_filtered
-plink -bfile CARTaGENE_hg38_shared_TOPMed_filtered --a1-allele CARTaGENE_hg38_shared_TOPMed_filtered.bim 6 2  --output-chr chrMT --recode vcf-iid bgz --out CARTaGENE_hg38_shared_TOPMed_filtered
+plink --bfile %{CaG%.*}  --keep-allele-order --freq --out %{CaG%.*} # Make Allele frequency
+python3 plink_freq_vs_topmed.py -s %{CaG%.*}.frq -t ${Bravo} -m 0.2 -o Diff_${Threshold} # Test frequency concordance
+plink --bfile %{CaG%.*} --exclude Diff_${Threshold}.af_diff.txt --keep-allele-order --make-bed --out %{CaG%.*}_TOPMed_filtered # Make Bed
+plink -bfile %{CaG%.*}_filtered --a1-allele %{CaG%.*}_TOPMed_filtered.bim 6 2  --output-chr chrMT --recode vcf-iid bgz --out %{CaG%.*}_TOPMed_filtered # make VCF
