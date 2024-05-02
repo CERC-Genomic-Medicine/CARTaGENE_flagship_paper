9d05d3006a04e482f6f71a1f823244c245604f2d
VChapdelaineT <96508889+VChapdelaineT@users.noreply.github.com>
2024-05-02 16:07:05 -0400

    Delete Genotype_processing/5_Harmonization/Remove_Variants.sh

--- Genotype_processing/5_Harmonization/Remove_Variants.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-plink -bfile CARTaGENE_hg38_shared --keep-allele-order  --output-chr chrMT --exclude Variants_failing_ancestry_LRTtest.txt --make-bed --out CARTaGENE_hg38_shared_AF_filtered
-plink -bfile CARTaGENE_hg38_shared --a1-allele CARTaGENE_hg38_shared.bim 6 2  --output-chr chrMT --exclude Variants_failing_ancestry_LRTtest.txt --recode vcf-iid bgz --out CARTaGENE_hg38_shared_AF_filtered
