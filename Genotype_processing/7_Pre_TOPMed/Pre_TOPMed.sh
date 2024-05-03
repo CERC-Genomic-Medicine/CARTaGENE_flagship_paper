6217e303c0e674ccbc15045e40d767012d22ae93
VChapdelaineT <96508889+VChapdelaineT@users.noreply.github.com>
2024-05-03 10:14:22 -0400

    Update Pre_TOPMed.sh

--- Genotype_processing/7_Pre_TOPMed/Pre_TOPMed.sh
+++ Genotype_processing/7_Pre_TOPMed/Pre_TOPMed.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
+# Parameters
+## File
+### Input file
+CaG="Path/to/file/*.bim"        # path to CAG PLINK binary format's bim file with AF concordant with TOPMed, set should contain a .bed .bim .fam file.
+### Variables
+Batch_length=25000              # Length of batches (limited by TOPMed imputation server)
 
-plink --bfile CARTaGENE_hg38_shared_TOPMed_filtered  --a1-allele CARTaGENE_hg38_shared_TOPMed_filtered.bim 6 2 --output-chr chrMT --export vcf-iid bgz --out CARTaGENE_hg38_shared_filtered_temp ; done
-bcftools index CARTaGENE_hg38_shared_filtered_temp.vcf.gz
-bcftools +fill-tags CARTaGENE_hg38_shared_filtered_temp.vcf.gz -Oz -o CARTaGENE_hg38_shared_filtered.vcf.gz
+plink --bfile ${CaG%.*}  --a1-allele ${CaG} 6 2 --output-chr chrMT --export vcf-iid bgz --out ${CaG%.*}_temp ; done
+bcftools index ${CaG%.*}_temp.vcf.gz
+bcftools +fill-tags ${CaG%.*}_temp.vcf.gz -Oz -o ${CaG%.*}_filtered.vcf.gz
 rm *temp.vcf.gz
-bcftools index CARTaGENE_hg38_shared_filtered.vcf.gz
-bcftools query -l CARTaGENE_hg38_shared_filtered.vcf.gz > samples
+bcftools index ${CaG%.*}_filtered.vcf.gz
+bcftools query -l ${CaG%.*}_filtered.vcf.gz > samples
 shuf samples > samples_random.txt
-head -n 25000 samples_random.txt > batch1.txt
-tail -n 25000 samples_random.txt > batch2.txt
+head -n ${Batch_length} samples_random.txt > batch1.txt
+tail -n ${Batch_length} samples_random.txt > batch2.txt
 
-for  i in chr{1..22} chrX ; do 
-  bcftools view -r $i CARTaGENE_hg38_shared_filtered.vcf.gz -S batch1.txt  -Oz -o CARTaGENE_hg38_shared_filtered_"$i"_batch1.vcf.gz; 
-  bcftools view -r $i CARTaGENE_hg38_shared_filtered.vcf.gz -S batch2.txt  -Oz -o CARTaGENE_hg38_shared_filtered_"$i"_batch2.vcf.gz; 
-  bcftools index CARTaGENE_hg38_shared_filtered_"$i"_batch1.vcf.gz ; 
-  bcftools index CARTaGENE_hg38_shared_filtered_"$i"_batch2.vcf.gz ; 
+for chromosome in chr{1..22} chrX ; do 
+  bcftools view -r ${chromosome} ${CaG%.*}_filtered.vcf.gz -S batch1.txt  -Oz -o ${CaG%.*}_filtered_${chromosome}_batch1.vcf.gz; 
+  bcftools view -r ${chromosome} ${CaG%.*}_filtered.vcf.gz -S batch2.txt  -Oz -o ${CaG%.*}_filtered_${chromosome}_batch2.vcf.gz; 
+  bcftools index ${CaG%.*}_filtered_${chromosome}_batch1.vcf.gz ; 
+  bcftools index ${CaG%.*}_filtered_${chromosome}_batch2.vcf.gz ; 
 done
