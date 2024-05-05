907a090d9d6b0a2d8f5235dec66366bec5e9bf24
VChapdelaineT <96508889+VChapdelaineT@users.noreply.github.com>
2024-05-05 15:22:02 -0400

    replaced

--- Genotype_processing/QC_Evaluation/Illustration_Harmonization/covar_PCA_prep.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-import pandas as pd
-SEX= pd.read_csv("SEX_ID", sep = "\s+", low_memory = False)
-PCA= pd.read_csv("CARTaGENE.eigenvec", sep = "\s+", low_memory = False)
-PCA.rename(columns = {'#IID': 'IID'},inplace=True)
-pd.merge(SEX,PCA,on="IID").to_csv('COVARIANT.file',sep='\t',index=False,header=True)
