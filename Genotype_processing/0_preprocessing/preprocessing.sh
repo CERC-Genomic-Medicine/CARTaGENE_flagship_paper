f8408c01f4bc4f0e45ef8f7962f024b175eb3360
VChapdelaineT <96508889+VChapdelaineT@users.noreply.github.com>
2024-05-27 10:53:09 -0400

    Update to Nextflow

--- Genotype_processing/0_preprocessing/preprocessing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-path=“/path/to/plink_files/*bim”  # path to CAG PLINK binary format's set, each set should contain a .bed .bim .fam file.
-remove_samples_file='/path/to/list_of_samples_to_remove/file.samples' # path to the list of samples to remove
-
-for bim in ${path}; do 
-        plink -bfile ${bim%.*} --remove ${remove_samples_file} --make-bed --output-chr MT --out ${bim%.*}_consented
--- Genotype_processing/5_Harmonization/Harmonization_test/Variable_prep.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import pandas as pd
-
-## Variable
-CAG = "Path/to/file/[file].ProPC.coord"        # path to trace output *.ProPC.coord
-
-df= pd.read_table(CaG,header=0,sep="\t",low_memory=False, usecols = ['indivID'] + [f'PC{i}' for i in range(1, 21)])
-df.rename(columns = {'indivID': "IID"}, inplace=True)
-df.to_csv('CARTaGENE_projection.PCA', sep= '\t', index=False)
